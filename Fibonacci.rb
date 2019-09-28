5648991f447d8b3921b08abc6e60fcf2f6e16249
Sophie <33546750+Pl4typusRex@users.noreply.github.com>
2019-09-28 16:10:30 +0100

    Initial commit
    Calculates and prints the first x numbers of the Fibonacci sequence where x is user input.

---
+++ Fibonacci.rb
@@ -0,0 +1,21 @@
+#Fibonacci sequence, such that each number is the sum of the two preceding ones
+
+# get user input to determine end point
+puts "This will calculate the first x numbers of the Fibonacci sequence\n"
+puts "Please enter a number: "
+nth_term = gets.chomp
+nth_term = nth_term.to_i
+
+sequence = [0,1]
+
+(1..nth_term).each do
+
+  point_1 = sequence[sequence.length()-2]
+  point_2 = sequence[sequence.length()-1]
+
+  x = point_1 + point_2
+  sequence.push(x)
+
+end
+
+puts "#{sequence}"
